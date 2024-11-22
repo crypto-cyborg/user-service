a2708db24f243aaa14c16df4d50c358853dc980b
tng <m.farm4nov@gmail.com>
2024-11-23 03:45:11 +0400

    storing binance keys

--- UserService.Core/Models/User.cs
+++ UserService.Core/Models/User.cs
@@ -6,9 +6,13 @@
 
         public string Username { get; set; }
         public string PasswordHash { get; set; }
+        public string ImageUrl { get; set; }
 
         public string Email { get; set; }
         public bool IsEmailConfirmed { get; set; } = false;
+
+        public string ApiKey { get; set; }
+        public string SecretKey { get; set; }
 
         public string FirstName { get; set; }
         public string LastName { get; set; }
---
+++ UserService.Persistence/Migrations/20241122205740_BinanceKeys.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UserService.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class BinanceKeys : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ApiKey",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecretKey",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApiKey",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SecretKey",
+                table: "Users");
+        }
+    }
+}
